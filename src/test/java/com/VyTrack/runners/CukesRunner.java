e872f2d06eb48c6e64d806e1c838f29b9e4829ea
SvitlanaPelekh <114306749+Svetapelekh@users.noreply.github.com>
2023-03-08 17:26:00 -0600

    Update CukesRunner.java

--- src/test/java/com/VyTrack/runners/CukesRunner.java
+++ src/test/java/com/VyTrack/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/VyTrack/step_definitions",
         dryRun = false,
-        tags = "@B28G8-132",
+        tags = "@wip",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
