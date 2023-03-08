92912e0cb0f7294ee1bf6e7a3a75893a99265cc4
Dana_git <usdanakl@gmail.com>
2023-03-08 17:06:18 -0600

    US05 changed tag

--- src/test/java/com/VyTrack/runners/CukesRunner.java
+++ src/test/java/com/VyTrack/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/VyTrack/step_definitions",
         dryRun = false,
-        tags = "@B28G8-140",
+//        tags = "@B28G8-140",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
