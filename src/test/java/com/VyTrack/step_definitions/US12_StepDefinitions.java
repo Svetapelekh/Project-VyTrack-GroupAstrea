f60dbf78170edd8b88fe16188488f6ce7d2b2dda
kobelev9 <kirill97kobelev@gmail.com>
2023-03-09 02:09:28 -0600

    Updated StepDefinitions for US12

--- src/test/java/com/VyTrack/step_definitions/US12_StepDefinitions.java
+++ src/test/java/com/VyTrack/step_definitions/US12_StepDefinitions.java
@@ -1,29 +1,34 @@
 package com.VyTrack.step_definitions;
 
+import com.VyTrack.pages.US12_MainPage;
+import com.VyTrack.utilities.BrowserUtils;
+import com.VyTrack.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US12_StepDefinitions {
 
+    US12_MainPage page = new US12_MainPage();
+    //WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
     @When("the user hovers over Customers dropdown and clicks Accounts button")
     public void the_user_hovers_over_customers_dropdown_and_clicks_accounts_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        BrowserUtils.sleep(5);
+        page.customersDropdown.click();
+        page.accountsButton.click();
+        BrowserUtils.sleep(5);
     }
-    @When("the user turns on Filters if they were turned off")
+    @When("the user turns on Filters")
     public void the_user_turns_on_filters_if_they_were_turned_off() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        page.filtersButton.click();
+        BrowserUtils.sleep(5);
     }
     @Then("the user sees Expected filter names")
-    public void the_user_sees_expected_filter_names(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void the_user_sees_expected_filter_names() {
+        Assert.assertTrue(US12_MainPage.filtersAreDisplayed());
     }
 }
