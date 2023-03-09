1e750977a777ae8ed101e8886c917c9f6dbca999
kobelev9 <kirill97kobelev@gmail.com>
2023-03-08 20:32:10 -0600

    Created StepDefinitions for US08

---
+++ src/test/java/com/VyTrack/step_definitions/US08_StepDefinitions.java
@@ -0,0 +1,29 @@
+package com.VyTrack.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US08_StepDefinitions {
+
+    @Given("User hovers over dropdown Activities and clicks button Calendar Events")
+    public void user_hovers_over_dropdown_activities_and_clicks_button_calendar_events() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks button Create Calendar Event")
+    public void user_clicks_button_create_calendar_event() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User clicks checkbox Repeat")
+    public void user_clicks_checkbox_repeat() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should see the number {int} by default in the Repeat Every input.")
+    public void user_should_see_the_number_by_default_in_the_repeat_every_input(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
