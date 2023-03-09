d1040e08b9781520b2e276bed535e96db6d6fbc3
kobelev9 <kirill97kobelev@gmail.com>
2023-03-08 21:59:04 -0600

    Updated StepDefinitions for US08

--- src/test/java/com/VyTrack/step_definitions/US08_StepDefinitions.java
+++ src/test/java/com/VyTrack/step_definitions/US08_StepDefinitions.java
@@ -1,29 +1,41 @@
 package com.VyTrack.step_definitions;
 
+import com.VyTrack.pages.US08_MainPage;
+import com.VyTrack.utilities.BrowserUtils;
+import com.VyTrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US08_StepDefinitions {
 
+    US08_MainPage page = new US08_MainPage();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+
     @Given("User hovers over dropdown Activities and clicks button Calendar Events")
     public void user_hovers_over_dropdown_activities_and_clicks_button_calendar_events() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        wait.until(ExpectedConditions.titleIs("Dashboard"));
+        page.dropdownActivities.click();
+        page.buttonCalendarEvents.click();
+        BrowserUtils.sleep(5);
     }
     @When("User clicks button Create Calendar Event")
     public void user_clicks_button_create_calendar_event() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        page.buttonCreateCalendarEvent.click();
+        BrowserUtils.sleep(5);
     }
     @When("User clicks checkbox Repeat")
     public void user_clicks_checkbox_repeat() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        page.checkboxRepeat.click();
     }
-    @Then("User should see the number {int} by default in the Repeat Every input.")
-    public void user_should_see_the_number_by_default_in_the_repeat_every_input(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("User should see the number 1 by default in the Repeat Every input.")
+    public void user_should_see_the_number_by_default_in_the_repeat_every_input() {
+        int actualInt = Integer.valueOf(page.windowRepeatEvery.getAttribute("value"));
+        Assert.assertEquals(actualInt, 1);
     }
 }
