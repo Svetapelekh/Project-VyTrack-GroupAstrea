37aef15ba4135389e25a5766e852a4e813418704
kobelev9 <kirill97kobelev@gmail.com>
2023-03-09 00:19:48 -0600

    Created StepDefinitions for US12

---
+++ src/test/java/com/VyTrack/step_definitions/US12_StepDefinitions.java
@@ -0,0 +1,29 @@
+package com.VyTrack.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US12_StepDefinitions {
+
+    @When("the user hovers over Customers dropdown and clicks Accounts button")
+    public void the_user_hovers_over_customers_dropdown_and_clicks_accounts_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user turns on Filters if they were turned off")
+    public void the_user_turns_on_filters_if_they_were_turned_off() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user sees Expected filter names")
+    public void the_user_sees_expected_filter_names(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/resources/features/US12.feature
+++ src/test/resources/features/US12.feature
@@ -1,9 +1,10 @@
+@US12
 Feature: As a user, I want to filter customers’ info on the Account page.
 
   Background: User is already in the log in page
     Given the user is on the login page
 
-  Scenario : US12AC01TC01 users should see 8 filter items on the Accounts page
+  Scenario: US12AC01TC01 users should see 8 filter items on the Accounts page
     Given the user logged in as "sales manager"
     When the user hovers over Customers dropdown and clicks Accounts button
     And the user turns on Filters if they were turned off
